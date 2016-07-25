b5da2822870726da5e8256eee1b029740f63daef
qmxie <qmxie@thoughtworks.com>
2016-07-25 17:26:45 +0800

    [#0] remove -jar

--- src/main/docker/entrypoint.sh
+++ src/main/docker/entrypoint.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/env bash
-java -Xmx512m -Djava.security.egd=file:/dev/./urandom \
-     -cp $JAVA_HOME/lib/*:/lean/java/lib/*:/auth-server.jar org.thoughtworks.lean.identity.AuthServer \
-     --spring.profiles.active=$ACTIVE_PROFILE
+java -Xmx512m -Djava.security.egd=file:/dev/./urandom -cp $JAVA_HOME/lib/*:/lean/java/lib/*:/auth-server.jar org.thoughtworks.lean.identity.AuthServer --spring.profiles.active=$ACTIVE_PROFILE
